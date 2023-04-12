58a7279fb5ddf11787e74eba9fb31a8adfa65e28
Mark <lohanmark@yahoo.co.uk>
2023-04-12 17:21:03 +0100

    updated datasetahaon.csv .. added functionality for chatbot to learn from user input

--- chatbot.py
+++ chatbot.py
@@ -8,20 +8,37 @@ with open('tokens/tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
 # Load the model
-model = tf.keras.models.load_model('models/model.h5')
+model = tf.keras.models.load_model('models/model_user_updated.h5')
 model.summary()
 
 # Get max_length from the model
 max_length = model.layers[0].input_shape[1]
 
+def user_update_model(question, answer, epochs=5):
+    question_seq = tokenizer.texts_to_sequences([question])[0]
+    question_seq = tf.keras.preprocessing.sequence.pad_sequences([question_seq], maxlen=max_length, padding='post')
+    answer_seq = tokenizer.texts_to_sequences([answer])[0]
+    answer_seq = tf.keras.preprocessing.sequence.pad_sequences([answer_seq], maxlen=max_length, padding='post')
+    model.fit(question_seq, np.expand_dims(answer_seq, axis=-1), epochs=epochs)
+
 # Define the function to generate the response
-def generate_response(question):
+def generate_response(question, min_confidence=0.5):
     question_seq = tokenizer.texts_to_sequences([question])[0]
     question_seq = tf.keras.preprocessing.sequence.pad_sequences([question_seq], maxlen=max_length, padding='post')
     prediction = model.predict(question_seq)[0]
     index = np.argmax(prediction, axis=-1)
+    confidence = np.mean([prediction[i, idx] for i, idx in enumerate(index) if idx > 0])
     response = ' '.join([tokenizer.index_word[i] for i in index if i > 0])
-    return response
+    print(confidence)
+
+    # Check if the confidence is above the threshold
+    if confidence > min_confidence:
+        return response
+    else:
+        return "I'm sorry, I don't have an answer for that."
+
+
+# return response
 
 # Test the chatbot
 while True:
@@ -30,3 +47,12 @@ while True:
         break
     response = generate_response(question)
     print('Bot:', response)
+
+    # Ask for user feedback
+    feedback = input('Is this response good or bad? (y/n): ')
+    if feedback == 'n':
+        proper_answer = input('Please provide the correct response: ')
+        user_update_model(question, proper_answer)
+
+model.save('models/model_user_updated.h5')
+print("model updated")
